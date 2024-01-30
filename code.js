3bd64f2b2a184e2a81cac3eefa81c86e3630d9ce
Isaiah Elston <156853632+isaiah-elston@users.noreply.github.com>
2024-01-30 22:42:13 +0000

    Attempt #1

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function insertionSortReverse(array) {
-    return array;
+    for (let i = array.length - 2; i >= 0; i--) {
+        let val = array[i];
+        let j;
+        for (j = i; j < array.length && array[j + 1] < val; j++) {
+            array[j] = array[j + 1];
+        }
+    array[j] = val;
+    }
+return array;
 }
